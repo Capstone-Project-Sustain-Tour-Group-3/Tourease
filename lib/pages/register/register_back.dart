412f99e7bc9e56bc157926c8cb7451ceb60f5af3
rafysp <rafysyah23@gmail.com>
2024-05-25 11:14:10 +0700

    feat: add RegisterBack widget for register page navigation

---
+++ lib/pages/register/register_back.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tourease/constants/color_constant.dart';
+
+class RegisterBack extends StatelessWidget {
+  final bool addSpace;
+
+  const RegisterBack({super.key, this.addSpace = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (addSpace) const SizedBox(height: 40), // Adjust height as needed
+        Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: IconButton(
+            onPressed: () {
+              Get.back();
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: ColorPrimary.primary500,
+            ),
+            style: ButtonStyle(
+              overlayColor: WidgetStateProperty.all(ColorNeutral.neutral100),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
