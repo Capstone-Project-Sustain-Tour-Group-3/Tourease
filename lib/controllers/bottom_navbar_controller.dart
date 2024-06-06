ae8610f658c7b45543b382bf7a4f69d4abac68d9
rafysp <rafysyah23@gmail.com>
2024-06-07 00:18:46 +0700

    add route recommendation page

--- lib/controllers/bottom_navbar_controller.dart
+++ lib/controllers/bottom_navbar_controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:tourease/pages/destinasi/destinasi_page.dart';
 import 'package:tourease/pages/home/home_page.dart';
 import 'package:tourease/pages/profil/profil_page.dart';
+import 'package:tourease/pages/route_recommendation/home_page/route_recommendation_page.dart';
 
 class BottomNavbarController extends GetxController {
   RxInt selectedIndex = 0.obs;
@@ -13,7 +14,9 @@ class BottomNavbarController extends GetxController {
         DestinasiPage(
           searchText: searchText.value,
         ),
+        RouteHomePage(),
         const ProfilPage(),
+        
       ];
 
   void getInitialIndex({required int index}) {
